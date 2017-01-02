39b6d14342e7b1299b4378805a5e49d2688bf308
Metin Demir <metmirr@users.noreply.github.com>
2017-01-02 13:24:45 +0000

    Create mysql_connect.py

---
+++ mysql_connect.py
@@ -0,0 +1,19 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='username',
+                             port=3306,
+                             password='password',
+                             db='testdb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+if __name__ == '__main__':
+	try:
+		with connection.cursor() as cursor:
+			cursor.execute("SELECT * FROM tablename")
+			r = cursor.fetchone()
+			print(r)
+	finally:
+		connection.close()
